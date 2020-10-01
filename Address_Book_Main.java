30dc670d1f50e9dbfb51ca11d2e20116586a6a30
VCHINIMI <vinaychinimilli@gmail.com>
2020-10-01 15:22:57 +0530

    [Vinay]Added: Welcome Message for Address Book

---
+++ Address_Book_Main.java
@@ -0,0 +1,7 @@
+package MyPackage.Vinay.Address_Book_New;
+
+public class Address_Book_Main {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+	}
+}
